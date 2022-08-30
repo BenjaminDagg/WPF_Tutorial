02bce334fd570c700dbc88dda98ea164ae27b740
Benjamin Dagg <bdagg@DiamondGame.com>
2022-08-30 16:37:28 -0700

    First commit

---
+++ App-XAML/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace App_XAML
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// 
+    /// -starting point of app
+    /// - .NET goes to thispage for start instructions then starts desired window
+    /// - This is where you subscribe to application events like start, unhandled exceptions
+    /// </summary>
+    public partial class App : Application
+    {
+
+        //Override StartupUri property of Application to manually set startup
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+            wnd.Title = "Something else";
+
+            wnd.Show();
+        }
+    }
+}
---
+++ TheWindow/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TheWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// 
+    /// - window class is root of application
+    /// - WPF app is combination of XAML file with <Window></Window> root and .cs (CodeBehind window file)
+    /// - class is partial because its combined with XAML to get full window (initialzecomponent)
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
